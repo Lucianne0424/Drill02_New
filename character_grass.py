34c0cd80ad769309bf713f5c352784d272c4401d
Lucianne0424 <pwj0424@naver.com>
2023-09-21 12:20:00 +0900

    그리기 함수 추가

--- character_grass.py
+++ character_grass.py
@@ -7,7 +7,11 @@ character = load_image('character.png')
 grass = load_image('grass.png')
 
 
-
+def render_frame(x,y):
+	clear_canvas_now()
+	grass.draw_now(400, 30)
+	character.draw_now(x, y)
+	delay(0.01)
 
 def run_circle():
 	print('CIRCLE')
@@ -16,23 +20,17 @@ def run_circle():
 	for deg in range(0, 360, 5):
 		x = cx + r * math.cos(math.radians(deg))
 		y = cy + r * math.sin(math.radians(deg))
-		clear_canvas_now()
-		grass.draw_now(400, 30)
-		character.draw_now(x,y)
-		delay(0.1)
+		render_frame(x, y)
 
 def run_rectangle():
 	print('RECTANGLE')
 	for x in range(50, 750+1, 5):
-		clear_canvas_now()
-		grass.draw_now(400, 30)
-		character.draw_now(x,90)
-		delay(0.1)
-	
+		render_frame(x, 90) 
+
 
 
 while True:
-	# run_circle()
+	run_circle()
 	run_rectangle()
 	break;
 
